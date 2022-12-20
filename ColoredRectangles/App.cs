7437e56547cf87c5052b0f71a3f774a7a972f70b
GET Academy <terje@getacademy.no>
2022-12-20 11:00:29 +0100

    Initial commit

---
+++ ColoredRectangles/App.cs
@@ -0,0 +1,42 @@
+﻿namespace ColoredRectangles
+{
+    internal class App
+    {
+        private readonly List<Rectangle> _rectangles;
+
+        public App()
+        {
+            _rectangles = new List<Rectangle>
+            {
+                new Rectangle(ConsoleColor.DarkBlue, "A", 10, 10,  30, 4),
+                new Rectangle(ConsoleColor.White, "B", 12, 12, 30, 5),
+                new Rectangle(ConsoleColor.Red, "C", 14, 14, 30, 6),
+            };
+        }
+
+        public void Run()
+        {
+            ShowRectangles();
+            //while (true)
+            //{
+            //    Console.Clear();
+            //    ShowRectangles();
+            //    //ShowMenu();
+            //    Console.Write("Hva vil du gjøre? ");
+            //    var cmd = Console.ReadLine();
+            //    if (cmd == "F")
+            //    {
+            //        //RemoveRectangle();
+            //    }
+            //}
+        }
+
+        private void ShowRectangles()
+        {
+            foreach (var rectangle in _rectangles)
+            {
+                rectangle.Show();
+            }
+        }
+    }
+}
---
+++ ColoredRectangles/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿namespace ColoredRectangles
+{
+    internal class Rectangle
+    {
+        private readonly ConsoleColor _color;
+        private readonly string _id;
+        private readonly int _x;
+        private readonly int _y;
+        private readonly int _width;
+        private readonly int _height;
+
+        public Rectangle(ConsoleColor color, string id, int x, int y, int width, int height)
+        {
+            _color = color;
+            _id = id;
+            _x = x;
+            _y = y;
+            _width = width;
+            _height = height;
+        }
+
+        public void Show()
+        {
+            for (var y = _y; y < _height + y; y++)
+            {
+                ShowRow(y);
+            }
+        }
+
+        private void ShowRow(int y)
+        {
+            Console.CursorLeft = _x;
+            Console.CursorTop = y;
+            Console.BackgroundColor = _color;
+            var text = y == _y ? _id : string.Empty;
+            Console.Write(text.PadRight(_width));
+        }
+    }
+}

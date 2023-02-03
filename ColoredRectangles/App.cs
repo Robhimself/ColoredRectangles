ff2c81789c42f56dc92949c91a169d82a754d23c
Robert Lysebo <robertlysebo@gmail.com>
2023-02-03 16:38:05 +0100

    Added functions and cleaned up some code

--- ColoredRectangles/App.cs
+++ ColoredRectangles/App.cs
@@ -1,4 +1,4 @@
-﻿namespace ColoredRectangles
+namespace ColoredRectangles
 {
     internal class App
     {
@@ -21,37 +21,54 @@
                 Console.Clear();
                 ShowRectangles();
                 ShowMenu();
-                Console.Write("Hva vil du gjøre? ");
-                var cmd = Console.ReadLine();
-                if (cmd == "F")
-                {
-                    Console.WriteLine("Hvilken firkant vil du gjøre dette med? ");
-                    var id = Console.ReadLine();
-                    RemoveRectangle(id);
-                }
 
-                if (cmd == "+")
+                var cmd = GetUserInput();
+                if (IsValidInput(cmd) == false)
                 {
-                    AddRectangle();
+                    Console.WriteLine($"\"{cmd}\" er ikke en gyldig kommando. ");
+                    Console.WriteLine("Trykk en tast for å fortsette...");
+                    Console.ReadLine();
+                    
+                    continue;
                 }
-
-                if (cmd == "M")
-                {
-                    MoveRectangleToTop();
-                }
+                
+                if (cmd is "+") AddRectangle();
+                if (cmd is "f" or "m") EditRectangle(cmd);
+                if (cmd is "x" or "exit") break; // Avslutter programmet
             }
+        }
+
+        private static string GetUserInput()
+        {
+            Console.Write("Hva vil du gjøre? ");
+            return Console.ReadLine().ToLower();
+        }
+
+        private static bool IsValidInput(string cmd)
+        {
+            if (cmd is "f" or "m" or "+" or "x" or "exit")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsValidRectangle(string id)
+        {
+            var idToCheck = GetRectangleIndex(id);
+            return idToCheck >= 0 && idToCheck < _rectangles.Count;
         }
 
         private void ShowMenu()
         {
-            var menuHeight = FindMenuHeight();
-            Console.CursorTop = menuHeight;
+            Console.CursorTop = _rectangles.Max(rect => rect.GetMaxHeight());
             Console.WriteLine();
             Console.WriteLine("F = Fjerne firkant");
-            Console.WriteLine("+ = Legg til firkant");
             Console.WriteLine("M = Flytt firkant til toppen");
+            Console.WriteLine("+ = Legg til firkant");
+            Console.WriteLine("X = For å avslutte programmet");
         }
-
+        
         private void RemoveRectangle(string id)
         {
             var rectangleIndex = GetRectangleIndex(id);
@@ -81,33 +98,47 @@
 
         private void AddRectangle()
         {
-            var newRectangle = new Rectangle(ConsoleColor.Green, "D", 8, 8, 10, 10);
+            /* Liten kladd for å generere random firkant. 
+             * 
+             * var color = 
+             * var id = 
+             * var x = _rectangles.Count + 4;
+             * var y = 
+             * var w = 
+             * var h = 
+             * var newRectangle = new Rectangle(ConsoleColor.Green, id, x, y, w, h);
+             * _rectangles.Add(newRectangle);
+             */
+            
+            var newRectangle = new Rectangle(ConsoleColor.Green, "D", 7, 7, 10, 10);
             _rectangles.Add(newRectangle);
         }
 
-        private void MoveRectangleToTop()
+        private void MoveRectangleToTop(string id)
         {
-            Console.WriteLine("Hvilken firkant vil du gjøre dette med? ");
-            string rectangleId = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(rectangleId))
-            {
-                MoveRectangleToTop();
-                return;
-            }
-
-            var rectangleIndex = GetRectangleIndex(rectangleId);
+            var rectangleIndex = GetRectangleIndex(id);
             var oldRectangle = _rectangles[rectangleIndex];
             
-            RemoveRectangle(rectangleId);
+            RemoveRectangle(id);
             _rectangles.Add(oldRectangle);
         }
 
-        private int FindMenuHeight()
+        private void EditRectangle(string cmd)
         {
-            var biggestHeight = _rectangles.Max(rect => rect.GetMaxHeight());
-            return biggestHeight;
-        }
+            Console.Write("Hvilken firkant vil du gjøre dette med?: ");
+            var id = Console.ReadLine();
 
+            while (IsValidRectangle(id) == false)
+            {
+                Console.Write($"\"{id}\" finnes ikke. Vennligst tast inn bokstaven til en av firkantene over (case sensitive): ");
+                id = Console.ReadLine();
+            }
+
+            if (cmd == "f")
+                RemoveRectangle(id);
+            else
+                MoveRectangleToTop(id);
+        }
+        
     }
 }
